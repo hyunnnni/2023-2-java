d5742c71bc51095918b59d022c0d0c2b4e93e900
hanadool1 <ansgksk97@naver.com>
2023-10-04 13:18:33 +0900

    second

--- src/main/java/com/green/day4/ch4/C_RandomValMission.java
+++ src/main/java/com/green/day4/ch4/C_RandomValMission.java
@@ -5,6 +5,7 @@ public class C_RandomValMission {
         int rVal = (int) (Math.random() * 6) + 5; //곱한 수는 -1한게 곱했을 때 최대값이 된다 (1.000-이 절대 안 나오기 때문)
 
         System.out.println(rVal);
+        System.out.println(rVal);
 
         int rVal2 = (int) (Math.random() * 10) + 14;
         System.out.println(rVal2);
