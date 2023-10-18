eb2b23adfe3cbe2c215bec3b04bafc84c5162060
hyun <kimkh8056k@gmail.com>
2023-10-18 19:38:27 +0900

    23/10/06 day14

--- src/main/java/com/green/day12/ch6/MethodExam4.java
+++ src/main/java/com/green/day12/ch6/MethodExam4.java
@@ -9,20 +9,19 @@ public class MethodExam4 {
         for(int i = 0; i< arr2.length; i++){
             arr2[i] = a[i]; //딥카피 하는 중
             arr2[i] += num;
-            //temp[i] = arr[i] + num; 으로 줄일 수 있다
+            //arr2[i] = a[i] + num; 으로 줄일 수 있다
         }
 
         return arr2;
 
     }
 
-    int[] plusOrigin (int[] a, int num){
+    public void plusOrigin (int[] a, int num){
         int [] arr2 = a;
         for(int i = 0; i< arr2.length; i++){
             arr2[i] += num;
         }
-
-        return arr2;
+        System.out.println(Arrays.toString(arr2));
 
     }
 
@@ -42,7 +41,7 @@ public class MethodExam4 {
         System.out.println(Arrays.toString(rArr)); //[12, 22, 32]
 
         me4.plusOrigin(arr,4); //return을 써도 되긴 한다 근데 구별을 할 수는 있어야 됨 난 실패~!
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(arr));
 
         int[] rArr2 = me4.randomValFromTo(10, 5, 20); //배열크기, 랜덤 min값, 랜덤 max값
         System.out.println(Arrays.toString(rArr2));
