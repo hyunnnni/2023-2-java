3339c88f13b7a3cfb82a2109172b16a699c50151
hyun <kimkh8056k@gmail.com>
2023-10-26 18:28:14 +0900

    23/10/25 day20

--- src/main/java/com/green/day15/ch7/CaptionTvTest2.java
+++ src/main/java/com/green/day15/ch7/CaptionTvTest2.java
@@ -3,11 +3,13 @@ package com.green.day15.ch7;
 public class CaptionTvTest2 {
     public static void main(String[] args) {
        CaptionTv2 ctv = new CaptionTv2();//객체 생성 하나로 상속한 부모 객체까지 생성됨(오브젝트까지)
-       ctv.channel = 10; //captionTv2의 channel이 10으로 된다
+       ctv.channel = 10;//captionTv2의 channel이 10으로 된다
+        ctv.getParentChannel();
        ctv.channelUp();// captionTv2에는 이 메소드가 없어서 그 부모인 Tv2에 가서 메소드를 호출하고 채널업을 한다. 이때 불러온 속성은 같은 클래스인
         //Tv2의 채널을 ++한것이다. 한 번 올라가면 자식클래스로 내려가진 않기 때문
         System.out.printf("ctv.channel:%d\n", ctv.channel);
         ctv.printParentChannel();
+
     }
 }
 
@@ -34,6 +36,6 @@ class CaptionTv2 extends Tv2 {
     }
 
     void getParentChannel(){
-        System.out.printf("ctv.channel : %d\n", channel);
+        System.out.printf("ctv.channel : %d\n", super.channel);
     }
 }
